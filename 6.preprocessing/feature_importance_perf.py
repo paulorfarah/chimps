d60025150b4192afa2675de5978b4ae3eed379bd
Paulo Roberto Farah <paulorfarah@gmail.com>
2026-06-07 08:19:49 -0300

    Preprocessing of dataset

---
+++ 6.preprocessing/feature_importance_perf.py
@@ -0,0 +1,256 @@
+from mdi import mdi
+from information_gain import information_gain_cpmp, information_gain_perf
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+
+def plot_ranking(df):
+    sns.set(font_scale=1.5)
+    df['rank'] = df['rank'].astype(int)
+    df1 = df[df['rank'] == 1]
+    df1 = df1.metric.value_counts()
+    print(df1.index)
+    plot = df1.plot.pie(y='0', autopct='%1.1f%%', startangle=90, figsize=(5, 5), ylabel='', xlabel='')
+#     plot = df1.plot.pie(y='metric', figsize=(5, 5))
+#     plt.axes().set_ylabel('')
+    plt.savefig('results/perf/top1.pdf')
+    # plt.close()
+
+    # top5
+
+    df1 = df[df['rank'] <= 5]
+    df1 = df1.metric.value_counts()
+    print(df1.index)
+    # plot = df1.plot.pie(y='0', autopct='%1.1f%%', startangle=90, figsize=(7, 7), textprops={'fontsize': 14})
+    plot = df1.plot.barh(figsize=(11, 5))
+
+    plt.xticks(range(1, df1.max() + 1))
+    plt.savefig('results/perf/top5.pdf')
+    plt.close()
+
+
+def plot_barh(df, filename):
+    # matplotlib.rcParams.update({'font.size': 16})
+    # sns.set(font_scale=1.2)
+    plt.figure(figsize=(8, 8))
+
+    graph = plt.barh(
+        df.index.to_list(),
+        df['count'],
+        color="lightsteelblue",
+    )
+    plt.xticks(range(0, df['count'].max() + 1))
+    # plt.xlim([0, 100])
+    i = 0
+    for p in graph:
+        width = p.get_width()
+        height = p.get_height()
+        x, y = p.get_xy()
+        plt.text(x + width,
+                 y + height * 0.3,
+                 str(df.percent[i]) + '%',
+                 ha='center')
+        i += 1
+    plt.tight_layout()
+    plt.savefig('results/perf/' + filename)
+
+    # courses = list(data.keys())
+    # values = list(data.values())
+    #
+    # fig = plt.figure(figsize=(10, 5))
+    #
+    # # creating the bar plot
+    # plt.barh(courses, values, color='maroon')
+    #
+    # plt.xlabel("Courses offered")
+    # plt.ylabel("No. of students enrolled")
+    # plt.title("Students enrolled in different courses")
+    # plt.show()
+
+
+def plot_pie(df, filename):
+    matplotlib.rcParams.update({'font.size': 16})
+    # sns.set(font_scale=1.2)
+    plt.figure(figsize=(8, 8))
+
+    plt.pie(
+        x=df['count'],
+        labels=df.index.to_list(),
+        # autopct='%1.2f%%',
+        # colors=sns.color_palette('magma'),
+        startangle=90,
+        # Add space around only one slice
+        # explode=[0, 0, 0, 0, 0.12, 0]
+    )
+    plt.savefig('results/perf/' + filename)
+
+
+if __name__ == "__main__":
+    # projects = ['commons-bcel', 'commons-csv', 'commons-io', 'easymock', 'Openfire', 'pdfbox', 'wro4j']
+    projects = ['commons-bcel', 'commons-csv', 'easymock', 'jgit', 'Openfire']
+    # projects = ['commons-bcel', 'commons-csv']
+    rank = 1
+    main_columns = [
+        # ck
+        "id",
+        'file', 'class', 'method', 'constructor', 'line', 'cbo', 'cboModified', 'fanin', 'fanout', 'wmc',
+        'rfc', 'loc', 'returnsQty', 'variablesQty', 'parametersQty', 'methodsInvokedQty',
+        'methodsInvokedLocalQty', 'methodsInvokedIndirectLocalQty', 'loopQty', 'comparisonsQty',
+        'tryCatchQty', 'parenthesizedExpsQty', 'stringLiteralsQty', 'numbersQty', 'assignmentsQty',
+        'mathOperationsQty', 'maxNestedBlocksQty', 'anonymousClassesQty', 'innerClassesQty', 'lambdasQty',
+        'uniqueWordsQty', 'modifiers', 'logStatementsQty', 'hasJavaDoc',
+
+        # added
+        'method_name', 'current_hash',
+        # understand
+        "Kind", "Name", "File", "AvgCyclomatic", "AvgCyclomaticModified", "AvgCyclomaticStrict",
+        "AvgEssential", "AvgLine", "AvgLineBlank", "AvgLineCode", "AvgLineComment", "CountClassBase",
+        "CountClassCoupled", "CountClassDerived", "CountDeclClass", "CountDeclClassMethod",
+        "CountDeclClassVariable", "CountDeclFile", "CountDeclFunction", "CountDeclInstanceMethod",
+        "CountDeclInstanceVariable", "CountDeclMethod", "CountDeclMethodAll", "CountDeclMethodDefault",
+        "CountDeclMethodPrivate", "CountDeclMethodProtected", "CountDeclMethodPublic", "CountInput",
+        "CountLine", "CountLineBlank", "CountLineCode", "CountLineCodeDecl", "CountLineCodeExe",
+        "CountLineComment", "CountOutput", "CountPath", "CountSemicolon", "CountStmt", "CountStmtDecl",
+        "CountStmtExe", "Cyclomatic", "CyclomaticModified", "CyclomaticStrict", "Essential",
+        "MaxCyclomatic", "MaxCyclomaticModified", "MaxCyclomaticStrict", "MaxEssential",
+        "MaxInheritanceTree", "MaxNesting", "PercentLackOfCohesion", "RatioCommentToCode",
+        "SumCyclomatic", "SumCyclomaticModified", "SumCyclomaticStrict", "SumEssential",
+
+        # evometrics
+        'project', 'commit',
+        'commitprevious', 'release', 'file', 'method', 'BOM', 'TACH', 'FCH', 'LCH', 'CHO',
+        'FRCH', 'CHD', 'WCH', 'WCD', 'WFR', 'ATAF', 'LCA', 'LCD', 'CSB', 'CSBS', 'ACDF',
+        # change distiller
+        "PROJECT_NAME", "PREVIOUS_COMMIT", "CURRENT_COMMIT", "CLASS_CURRENTCOMMIT", "CLASS_PREVIOUSCOMMIT",
+        "CLASS_CURRENTNAME", "CLASS_PREVIOUSNAME",
+        "STATEMENT_DELETE", "STATEMENT_INSERT", "STATEMENT_ORDERING_CHANGE",
+        "STATEMENT_PARENT_CHANGE", "STATEMENT_UPDATE", "TOTAL_STATEMENTLEVELCHANGES",
+        "PARENT_CLASS_CHANGE", "PARENT_CLASS_DELETE", "PARENT_CLASS_INSERT", "CLASS_RENAMING",
+        "TOTAL_CLASSDECLARATIONCHANGES",
+        "RETURN_TYPE_CHANGE", "RETURN_TYPE_DELETE", "RETURN_TYPE_INSERT", "METHOD_RENAMING",
+        "PARAMETER_DELETE", "PARAMETER_INSERT", "PARAMETER_ORDERING_CHANGE", "PARAMETER_RENAMING",
+        "PARAMETER_TYPE_CHANGE", "TOTAL_METHODDECLARATIONSCHANGES",
+        "ATTRIBUTE_RENAMING", "ATTRIBUTE_TYPE_CHANGE", "TOTAL_ATTRIBUTEDECLARATIONCHANGES",
+        "ADDING_ATTRIBUTE_MODIFIABILITY", "REMOVING_ATTRIBUTE_MODIFIABILITY",
+        "REMOVING_CLASS_DERIVABILITY", "REMOVING_METHOD_OVERRIDABILITY",
+        "ADDING_CLASS_DERIVABILITY", "ADDING_CLASS_DERIVABILITY", "ADDING_METHOD_OVERRIDABILITY",
+        "TOTAL_DECLARATIONPARTCHANGES", "TOTAL_CHANGES", "will_change", "perf_change"
+    ]
+    #main_columns: file, class ,method, constructor, line, cbo, cboModified, fanin, fanout, wmc, rfc, loc, returnsQty, variablesQty, parametersQty, methodsInvokedQty, methodsInvokedLocalQty, methodsInvokedIndirectLocalQty, loopQty, comparisonsQty, tryCatchQty, parenthesizedExpsQty, stringLiteralsQty, numbersQty, assignmentsQty, mathOperationsQty, maxNestedBlocksQty, anonymousClassesQty, innerClassesQty, lambdasQty, uniqueWordsQty, modifiers, logStatementsQty, hasJavaDoc, method_name, current_hash, Kind, Name, File, AvgCyclomatic, AvgCyclomaticModified, AvgCyclomaticStrict, AvgEssential, AvgLine, AvgLineBlank, AvgLineCode, AvgLineComment, CountClassBase, CountClassCoupled, CountClassDerived, CountDeclClass, CountDeclClassMethod, CountDeclClassVariable, CountDeclFile, CountDeclFunction, CountDeclInstanceMethod, CountDeclInstanceVariable, CountDeclMethod, CountDeclMethodAll, CountDeclMethodDefault, CountDeclMethodPrivate, CountDeclMethodProtected, CountDeclMethodPublic, CountInput, CountLine, CountLineBlank, CountLineCode, CountLineCodeDecl, CountLineCodeExe, CountLineComment, CountOutput, CountPath, CountSemicolon, CountStmt, CountStmtDecl, CountStmtExe, Cyclomatic, CyclomaticModified, CyclomaticStrict, Essential, MaxCyclomatic, MaxCyclomaticModified, MaxCyclomaticStrict, MaxEssential, MaxInheritanceTree, MaxNesting, PercentLackOfCohesion, RatioCommentToCode, SumCyclomatic, SumCyclomaticModified, SumCyclomaticStrict, SumEssential, project, commit, commitprevious, release, file, method, BOC, TACH, FCH, LCH, CHO, FRCH, CHD, WCH, WCD, WFR, ATAF, LCA, LCD, CSB, CSBS, ACDF, PROJECT_NAME, PREVIOUS_COMMIT, CURRENT_COMMIT, CLASS_CURRENTCOMMIT, CLASS_PREVIOUSCOMMIT, CLASS_CURRENTNAME, CLASS_PREVIOUSNAME, STATEMENT_DELETE, STATEMENT_INSERT, STATEMENT_ORDERING_CHANGE, STATEMENT_PARENT_CHANGE, STATEMENT_UPDATE, TOTAL_STATEMENTLEVELCHANGES, PARENT_CLASS_CHANGE, PARENT_CLASS_DELETE, PARENT_CLASS_INSERT, CLASS_RENAMING, TOTAL_CLASSDECLARATIONCHANGES, RETURN_TYPE_CHANGE, RETURN_TYPE_DELETE, RETURN_TYPE_INSERT, METHOD_RENAMING, PARAMETER_DELETE, PARAMETER_INSERT, PARAMETER_ORDERING_CHANGE, PARAMETER_RENAMING, PARAMETER_TYPE_CHANGE, TOTAL_METHODDECLARATIONSCHANGES, ATTRIBUTE_RENAMING, ATTRIBUTE_TYPE_CHANGE, TOTAL_ATTRIBUTEDECLARATIONCHANGES, ADDING_ATTRIBUTE_MODIFIABILITY, REMOVING_ATTRIBUTE_MODIFIABILITY, REMOVING_CLASS_DERIVABILITY, REMOVING_METHOD_OVERRIDABILITY, ADDING_CLASS_DERIVABILITY, ADDING_CLASS_DERIVABILITY, ADDING_METHOD_OVERRIDABILITY, TOTAL_DECLARATIONPARTCHANGES, TOTAL_CHANGES, will_change
+
+    structural_metrics = ['line', 'cbo', 'cboModified', 'fanin', 'fanout', 'wmc',
+                          'rfc', 'loc', 'returnsQty', 'variablesQty', 'parametersQty', 'methodsInvokedQty',
+                          'methodsInvokedLocalQty', 'methodsInvokedIndirectLocalQty', 'loopQty', 'comparisonsQty',
+                          'tryCatchQty', 'parenthesizedExpsQty', 'stringLiteralsQty', 'numbersQty', 'assignmentsQty',
+                          'mathOperationsQty', 'maxNestedBlocksQty', 'anonymousClassesQty', 'innerClassesQty',
+                          'lambdasQty',
+                          'uniqueWordsQty', 'modifiers', 'logStatementsQty', 'hasJavaDoc',
+                          "AvgCyclomatic", "AvgCyclomaticModified", "AvgCyclomaticStrict",
+                          "AvgEssential", "AvgLine", "AvgLineBlank", "AvgLineCode", "AvgLineComment", "CountClassBase",
+                          "CountClassCoupled", "CountClassDerived", "CountDeclClass", "CountDeclClassMethod",
+                          "CountDeclClassVariable", "CountDeclFile", "CountDeclFunction", "CountDeclInstanceMethod",
+                          "CountDeclInstanceVariable", "CountDeclMethod", "CountDeclMethodAll",
+                          "CountDeclMethodDefault",
+                          "CountDeclMethodPrivate", "CountDeclMethodProtected", "CountDeclMethodPublic", "CountInput",
+                          "CountLine", "CountLineBlank", "CountLineCode", "CountLineCodeDecl", "CountLineCodeExe",
+                          "CountLineComment", "CountOutput", "CountPath", "CountSemicolon", "CountStmt",
+                          "CountStmtDecl",
+                          "CountStmtExe", "Cyclomatic", "CyclomaticModified", "CyclomaticStrict", "Essential",
+                          "MaxCyclomatic", "MaxCyclomaticModified", "MaxCyclomaticStrict", "MaxEssential",
+                          "MaxInheritanceTree", "MaxNesting", "PercentLackOfCohesion", "RatioCommentToCode",
+                          "SumCyclomatic", "SumCyclomaticModified", "SumCyclomaticStrict", "SumEssential"]
+    evolutionary_metrics = [
+        'BOM', 'TACH', 'FCH', 'LCH', 'CHO', 'FRCH', 'CHD', 'WCH', 'WCD', 'WFR', 'ATAF', 'LCA', 'LCD', 'CSB', 'CSBS',
+        'ACDF',
+    ]
+    # change_distiller_metrics = [
+    #     "TOTAL_CHANGES",
+    #     "will_change",
+    #     "perf_change"
+    # ]
+
+    model1 = structural_metrics
+    model2 = structural_metrics + evolutionary_metrics
+    model3 = evolutionary_metrics
+
+    ### mdi
+    res = mdi(projects, main_columns, model2)
+    res['rank'] = res['rank'].astype(int)
+    # df1 = res[res['rank'] == 1]
+    # df1 = pd.DataFrame(df1.metric.value_counts())
+    # df1.columns = ['count']
+    # print(df1.index)
+    #
+    # df1['percent'] = (df1['count'] /
+    #                   df1['count'].sum()) * 100
+    # plot_pie(df1, 'mdi-top1.pdf')
+    # df1.to_csv('results/perf/mdi-df1.csv')
+    #
+    # df5 = res[res['rank'] <= 5]
+    # df5 = pd.DataFrame(df5.metric.value_counts())
+    # df5.columns = ['count']
+    # # print(df1.index)
+    #
+    # df5['percent'] = (df5['count'] /
+    #                   df5['count'].sum()) * 100
+    # df5['percent'] = df5['percent'].round(1)
+    # df5.sort_values(by=['percent'], ascending=False)
+    # df5 = df5.rename_axis('metrics').sort_values(by=['percent', 'metrics'], ascending=[True, False])
+    # plot_barh(df5, 'mdi-top5.pdf')
+    # df5.to_csv('results/perf/mdi-top5.csv')
+
+    df5 = res[res['rank'] <= rank]
+    df5 = pd.DataFrame(df5.metric.value_counts())
+    df5.columns = ['count']
+    # print(df1.index)
+
+    df5['percent'] = (df5['count'] /
+                      df5['count'].sum()) * 100
+    df5['percent'] = df5['percent'].round(1)
+    df5.sort_values(by=['percent'], ascending=False)
+    df5 = df5.rename_axis('metrics').sort_values(by=['percent', 'metrics'], ascending=[True, False])
+    plot_barh(df5, 'mdi-top' + str(rank) + '.pdf')
+    df5.to_csv('results/perf/mdi-top' + str(rank) + '.csv')
+
+
+    ### information gain
+    res = information_gain_perf(projects, main_columns, model2)
+
+    res['rank'] = res['rank'].astype(int)
+    # df1 = res[res['rank'] == 1]
+    # df1 = pd.DataFrame(df1.metric.value_counts())
+    # df1.columns = ['count']
+    # # print(df1.index)
+    #
+    #
+    # df1['percent'] = (df1['count'] /
+    #                   df1['count'].sum()) * 100
+    # plot_pie(df1, 'infgain-top1.pdf')
+    # df1.to_csv('results/perf/infgian-top1.csv')
+    #
+    # df5 = res[res['rank'] <= 5]
+    # df5 = pd.DataFrame(df5.metric.value_counts())
+    # df5.columns = ['count']
+    # # print(df1.index)
+    #
+    # df5['percent'] = (df5['count'] /
+    #                   df5['count'].sum()) * 100
+    # df5['percent'] = df5['percent'].round(1)
+    # df5.sort_values(by=['percent'], ascending=False)
+    # df5 = df5.rename_axis('metrics').sort_values(by=['percent', 'metrics'], ascending=[True, False])
+    # plot_barh(df5, 'infgain-top5.pdf')
+    # df5.to_csv('results/perf/infgain-top5.csv')
+
+    df5 = res[res['rank'] <= rank]
+    df5 = pd.DataFrame(df5.metric.value_counts())
+    df5.columns = ['count']
+    # print(df1.index)
+
+    df5['percent'] = (df5['count'] /
+                      df5['count'].sum()) * 100
+    df5['percent'] = df5['percent'].round(1)
+    df5.sort_values(by=['percent'], ascending=False)
+    df5 = df5.rename_axis('metrics').sort_values(by=['percent', 'metrics'], ascending=[True, False])
+    plot_barh(df5, 'infgain-top' + str(rank) + '.pdf')
+    df5.to_csv('results/perf/infgain-top' + str(rank) + '.csv')
---
+++ 6.preprocessing/information_gain.py
@@ -0,0 +1,127 @@
+import matplotlib
+import pandas as pd
+import re
+import numpy as np
+
+import category_encoders as ce
+import seaborn as sns
+
+from sklearn.feature_selection import SelectKBest, SelectPercentile, mutual_info_classif
+import matplotlib.pyplot as plt
+
+def weka_tokenizer(doc):
+    delimiters_regexp = re.compile("[ |\n|\f|\r|\t|.|,|;|:|'|\"|(|)|?|!]")
+    # delimiters_regexp = re.compile("[ -\/:-@\[-\`{-~|0-9|\n|\f|\r|\t|\s]")
+    return list(filter(None, delimiters_regexp.split(doc)))
+
+def information_gain_cpmp(databases, main_columns, feature_names):
+    res = pd.DataFrame()
+    for db in databases:
+        all_releases_df = pd.read_csv(
+            '../6.join_metrics/results/' + db + '-all-releases.csv')#, usecols=main_columns)
+        all_releases_df.columns = main_columns
+        all_releases_df = all_releases_df.fillna(0)
+
+        df_filt = all_releases_df[feature_names]
+        print(df_filt.columns)
+
+        # total_data_X = np.array(all_releases_df[feature_names].copy())
+        total_data_X = np.array(df_filt.copy())
+        total_data_Y = np.array(pd.DataFrame(all_releases_df.loc[:, 'will_change']))
+
+        encoder = ce.LeaveOneOutEncoder(return_df=True)
+        X = encoder.fit_transform(total_data_X, total_data_Y)
+
+        selector = SelectKBest(mutual_info_classif, k=10)
+        X_reduced = selector.fit_transform(X, total_data_Y)
+        print(X_reduced.shape)
+
+        informationGain = dict(zip(X.columns, mutual_info_classif(X, total_data_Y, discrete_features=True)))
+        sortedInformationGain = sorted(informationGain, key=informationGain.get, reverse=True)
+        i = 0
+        sortedInformationGainPosition = []
+
+        for r in sortedInformationGain:
+            infGain = {
+                'position': i,
+                # 'metric': all_releases_df.columns[r],
+                'metric': df_filt.columns[r],
+                'information_gain': informationGain[r],
+                'total_occurences': len(X[X[r] > 0])
+            }
+
+            sortedInformationGainPosition.append(infGain)
+            i += 1
+
+            # print(i, all_releases_df.columns[r], informationGain[r])
+            print(i, df_filt.columns[r], informationGain[r])
+
+        infGainCSV = pd.DataFrame(sortedInformationGainPosition,
+                                  columns=['position', 'metric', 'information_gain', 'total_ocurences'])
+        infGainCSV['rank'] = infGainCSV['information_gain'].rank(method='dense', ascending=False)
+        infGainCSV['application'] = db
+        res = pd.concat([res, infGainCSV])
+        infGainCSV.to_csv('results/cpmp/information_gain_' + db + '-perf.csv', index=False)
+
+
+        cols = selector.get_support(indices=True)
+        selected_columns = X.iloc[:, cols].columns.tolist()
+    # plot_ranking(res)
+    return res
+
+
+
+def information_gain_perf(databases, main_columns, feature_names):
+    res = pd.DataFrame()
+    for db in databases:
+        all_releases_df = pd.read_csv(
+        '../6.join_metrics/results/' + db + '-perf-diff-all.csv')
+        all_releases_df.columns = main_columns
+        all_releases_df = all_releases_df.fillna(0)
+
+        df_filt = all_releases_df[feature_names]
+        print(df_filt.columns)
+
+        # total_data_X = np.array(all_releases_df[feature_names].copy())
+        total_data_X = np.array(df_filt.copy())
+        total_data_Y = np.array(pd.DataFrame(all_releases_df.loc[:, 'perf_change']))
+
+        encoder = ce.LeaveOneOutEncoder(return_df=True)
+        X = encoder.fit_transform(total_data_X, total_data_Y)
+
+        selector = SelectKBest(mutual_info_classif, k=10)
+        X_reduced = selector.fit_transform(X, total_data_Y)
+        print(X_reduced.shape)
+
+        informationGain = dict(zip(X.columns, mutual_info_classif(X, total_data_Y, discrete_features=True)))
+        sortedInformationGain = sorted(informationGain, key=informationGain.get, reverse=True)
+        i = 0
+        sortedInformationGainPosition = []
+
+        for r in sortedInformationGain:
+            infGain = {
+                'position': i,
+                # 'metric': all_releases_df.columns[r],
+                'metric': df_filt.columns[r],
+                'information_gain': informationGain[r],
+                'total_occurences': len(X[X[r] > 0])
+            }
+
+            sortedInformationGainPosition.append(infGain)
+            i += 1
+
+            # print(i, all_releases_df.columns[r], informationGain[r])
+            print(i, df_filt.columns[r], informationGain[r])
+
+        infGainCSV = pd.DataFrame(sortedInformationGainPosition,
+                                  columns=['position', 'metric', 'information_gain', 'total_ocurences'])
+        infGainCSV['rank'] = infGainCSV['information_gain'].rank(method='dense', ascending=False)
+        infGainCSV['application'] = db
+        res = pd.concat([res, infGainCSV])
+        infGainCSV.to_csv('results/perf/information_gain_' + db + '-perf.csv', index=False)
+
+
+        cols = selector.get_support(indices=True)
+        selected_columns = X.iloc[:, cols].columns.tolist()
+    # plot_ranking(res)
+    return res
